1e169b27f949125373aabc102861afee51270305
bre <iwanttosignup321@gmail.com>
2023-09-08 13:25:16 +0800

    init

---
+++ DemoInterface/Interfaces.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoInterface
+{
+    internal class Interfaces
+    {
+    }
+    public interface IBirth
+    {
+        string Birth();
+        string GetBirthDate();
+    }
+    public interface IWalk
+    {
+        string GetWalkSpeed();
+    }
+
+    public interface ITalk:IBirth
+    {
+        string GetLanguage();
+    }
+
+}
